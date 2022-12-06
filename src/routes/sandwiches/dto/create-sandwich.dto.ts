02e9ad60dfe0933f4a7826009b7dd02394b358b5
async-devil <async.devil@gmail.com>
2022-12-06 20:40:30 +0200

    feat: delete sandwiches route

--- src/routes/sandwiches/dto/create-sandwich.dto.ts
+++
@@ -1,6 +0,0 @@
-import { Type } from "@sinclair/typebox";
-
-export const CreateSandwichBodyDto = Type.Object({
-	name: Type.String(),
-	ingredients: Type.Record(Type.String(), Type.Number()),
-});
--- src/routes/sandwiches/dto/sandwich.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Static, Type } from "@sinclair/typebox";
-
-export const SandwichDto = Type.Object({
-	id: Type.Number(),
-	name: Type.String(),
-	ingredients: Type.Record(Type.String(), Type.Number()),
-});
-
-export type SandwichType = Static<typeof SandwichDto>;
