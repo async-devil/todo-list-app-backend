292e6a7fa6fddf947709a52089579b84681b1c4d
async-devil <async.devil@gmail.com>
2022-12-06 23:05:01 +0200

    test: add pg-client mock type

--- test/mocks/pg-client.mock.ts
+++ test/mocks/pg-client.mock.ts
@@ -1,4 +1,8 @@
+import { QueryResult } from "pg";
+
+type Response = Partial<QueryResult<unknown>>;
+
 export const pgClientMock = {
 	release: jest.fn(),
-	query: jest.fn(),
+	query: jest.fn<Promise<Response>, []>(),
 };
